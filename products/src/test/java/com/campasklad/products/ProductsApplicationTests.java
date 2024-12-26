c3397b9ed9bedd1f3f0b909535aec7dcb71c9125
azakyd <azamat.kydyraliev@gmial.com>
2024-12-26 14:10:18 +0600

    Initial commit

---
+++ products/src/test/java/com/campasklad/products/ProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campasklad.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
