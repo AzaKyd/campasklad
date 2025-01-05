2875930810a9a11aa6d6a5e8929039e66ce6e662
Азамат <azamat.kydyraliev@gmail.com>
2025-01-05 22:43:36 +0600

    05 01 2025 get posting products

--- facility/src/main/java/com/campasklad/facility/controller/WriteoffController.java
+++ facility/src/main/java/com/campasklad/facility/controller/WriteoffController.java
@@ -1,9 +1,14 @@
 package com.campasklad.facility.controller;
 
 import com.campasklad.facility.dto.PostingDto;
+import com.campasklad.facility.dto.WriteoffDto;
 import com.campasklad.facility.dto.filter.PostingFilterDto;
+import com.campasklad.facility.dto.filter.WriteoffFilterDto;
 import com.campasklad.facility.dto.request.PostingProductRequestDto;
+import com.campasklad.facility.dto.request.WriteoffProductRequestDto;
+import com.campasklad.facility.entity.Writeoff;
 import com.campasklad.facility.service.PostingService;
+import com.campasklad.facility.service.WriteoffService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -17,42 +22,41 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/api/facility")
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
-public class PostingController {
-
-    PostingService postingService;
+public class WriteoffController {
+    WriteoffService writeoffService;
 
     @GetMapping("/get-posting/{id}")
-    public ResponseEntity<PostingProductRequestDto> getPosting(@PathVariable Long id) {
-        return ResponseEntity.ok().body(postingService.getPostingById(id));
+    public ResponseEntity<WriteoffProductRequestDto> getPosting(@PathVariable Long id) {
+        return ResponseEntity.ok().body(writeoffService.getWriteoffById(id));
     }
 
     @GetMapping("/delete")
-    public ResponseEntity<PostingProductRequestDto> deletePosting(@RequestParam Long id) {
-        postingService.deletePosting(id);
+    public ResponseEntity<WriteoffProductRequestDto> deletePosting(@RequestParam Long id) {
+        writeoffService.deleteWriteoff(id);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/filter-posting")
-    public ResponseEntity<Page<PostingDto>> filterPosting(@RequestBody PostingFilterDto productFilterDto,
+    public ResponseEntity<Page<WriteoffDto>> filterPosting(@RequestBody WriteoffFilterDto writeoffFilterDto,
                                                            @PageableDefault Pageable pageable) {
-        return ResponseEntity.ok().body(postingService.getFilteredPosting(productFilterDto, pageable));
+        return ResponseEntity.ok().body(writeoffService.getFilteredWriteoff(writeoffFilterDto, pageable));
     }
 
     @PostMapping("/create-posting")
-    public ResponseEntity<Void> createPosting(@RequestBody PostingProductRequestDto postingProductRequestDto) {
-        postingService.createPostingProduct(postingProductRequestDto);
+    public ResponseEntity<Void> createPosting(@RequestBody WriteoffProductRequestDto writeoffProductRequestDto) {
+        writeoffService.createWriteoff(writeoffProductRequestDto);
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/update-posting")
-    public ResponseEntity<Void> updatePosting(@RequestBody PostingProductRequestDto postingProductRequestDto) {
-        postingService.updatePosting(postingProductRequestDto);
+    public ResponseEntity<Void> updatePosting(@RequestBody WriteoffProductRequestDto writeoffProductRequestDto) {
+        writeoffService.updateWriteoff(writeoffProductRequestDto);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/approve")
     public ResponseEntity<Void> approvePosting(@RequestParam Long id) {
-        postingService.approvePosting(id);
+        writeoffService.approveWriteoff(id);
         return ResponseEntity.ok().build();
     }
 }
